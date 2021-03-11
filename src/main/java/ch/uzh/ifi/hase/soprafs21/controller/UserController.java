5a3ae2a19615b1c6967dff30baaccdae01e8dbd2
Stefanie-K <71380307+Stefanie-K@users.noreply.github.com>
2021-03-11 15:55:26 +0100

    ADD:everything new

--- src/main/java/ch/uzh/ifi/hase/soprafs21/controller/UserController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//...
 /**
  * User Controller
  * This class is responsible for handling all REST request that are related to the user.
